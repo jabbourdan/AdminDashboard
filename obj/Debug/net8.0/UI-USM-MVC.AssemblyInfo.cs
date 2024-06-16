a76999f01f43e5d8909f6ec31ec34e23e788522b
Jabbour.Dandan <jaboordandan14@gmail.com>
2024-06-16 21:55:57 +0300

    Add dropdown for the sidebar marketing

--- obj/Debug/net8.0/UI-USM-MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/UI-USM-MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI-USM-MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67fd8035f2058215c1aa7b92c785b78c529127f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cfca02e15de4def5cb655a446d1040b67ec0191")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI-USM-MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI-USM-MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
